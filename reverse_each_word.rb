c113f6c9ecdcbe8abc72db8fcf8411ed3a3a926d
Julie  McManus <julieannesays@gmail.com>
2020-01-24 20:15:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.split.collect { |word| word.reverse }
+  reversed_sentence = sentence.split.collect { |word| word.reverse }
+  reversed_sentence.join(" ")
+
+
 end
